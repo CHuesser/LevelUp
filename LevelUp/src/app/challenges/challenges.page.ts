dc31d7a737ad0bb46f784073c0d6ae81e117ad13
Cloe <cloe.huesser@gmail.com>
2020-09-19 00:43:32 +0200

    make it nice 1.0

--- LevelUp/src/app/challenges/challenges.page.ts
+++ LevelUp/src/app/challenges/challenges.page.ts
@@ -12,6 +12,7 @@ export class ChallengesPage implements OnInit {
     private filteredChallenges: Challenge[];
     private challenges: Challenge[];
     private filterargs: number[];
+    private buttonColors: string[];
 
     ngOnInit() {
     }
@@ -22,6 +23,7 @@ export class ChallengesPage implements OnInit {
             this.filteredChallenges = challenges;
         }));
         this.filterargs = [0, 1, 2, 3];
+        this.buttonColors = ['danger', 'success', 'warning', 'tertiary'];
     }
 
     removeAddFilter(filter: number) {
@@ -29,12 +31,13 @@ export class ChallengesPage implements OnInit {
 
         if (index !== -1) {
             this.filterargs.splice(index, 1);
-
-            document.getElementById('filter_' + filter).setAttribute('color', 'medium');
+            // document.getElementById('filter_' + filter).setAttribute('color', 'medium');
+            document.getElementById('filter_' + filter).classList.add('deactivated-filter');
         } else {
             this.filterargs.push(filter);
-            document.getElementById('filter_' + filter).removeAttribute('color');
-
+            // document.getElementById('filter_' + filter).removeAttribute('color');
+            // document.getElementById('filter_' + filter).setAttribute('color', this.buttonColors[filter]);
+            document.getElementById('filter_' + filter).classList.remove('deactivated-filter');
         }
         this.filteredChallenges = this.challenges.filter(item => this.filterargs.indexOf(item.category) !== -1);
 
