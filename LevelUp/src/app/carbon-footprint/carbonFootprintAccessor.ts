eabd5e0aec7c06dfafc2fafdf194b90daa923beb
Cloe <cloe.huesser@gmail.com>
2020-09-19 17:58:40 +0200

    puuuuush

--- LevelUp/src/app/carbon-footprint/carbonFootprintAccessor.ts
+++ LevelUp/src/app/carbon-footprint/carbonFootprintAccessor.ts
@@ -1,27 +1,41 @@
-import {CarbonFootprintArgs} from "./carbonFootprintArgs";
+import {CarbonFootprintArgs} from './carbonFootprintArgs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
+
+@Injectable({
+    providedIn: 'root'
+})
 
 export class CarbonFootprintAccessor {
-    apiPrefix = 'https://api.triptocarbon.xyz/v1/footprint?';
+    apiPrefix = 'https://cors-anywhere.herokuapp.com/https://api.triptocarbon.xyz/v1/footprint?';
+
+    constructor(public http: HttpClient) {
+
+    }
 
 
-    getCarbonFootprint(args: CarbonFootprintArgs): Promise<CarbonFootprintResponse>{
+    getCarbonFootprint(args: CarbonFootprintArgs) {
         // example: https://api.triptocarbon.xyz/v1/footprint?activity=10&activityType=miles&country=def&mode=taxi
-        const url = new URL(this.apiPrefix + 'activity=' + args.activity +
-            '&activityType=' + args.activityType + '&country=' + args.country + '&mode=' + args.mode);
+        // https://api.triptocarbon.xyz/v1/footprint?activity=20&activityType=miles&country=def&mode=petrolCar
+        const url = this.apiPrefix + 'activity=' + args.activity +
+            '&activityType=' + args.activityType + '&country=' + args.country + '&mode=' + args.mode;
 
-        return fetch(url.toString(),{
-                mode: 'no-cors',
-                headers: {
-                    "Content-Type": "application/json"
-                },
-            }
-            ).then(res => res.json()).then(res => res as CarbonFootprintResponse);
+        // tslint:disable-next-line:max-line-length
+        return this.http.get(url).toPromise().then(res => console.log(res));
+
+//
+        //return fetch(url.toString(),{
+        //        mode: 'no-cors',
+        //        headers: {
+        //            "Content-Type": "application/json"
+        //        },
+        //    }
+        //    ).then(res => res.json()).then(res => res as CarbonFootprintResponse);
     }
 }
 
 
-
-export class CarbonFootprintResponse{
+export class CarbonFootprintResponse {
     carbonFootprint: number;
 }
--- LevelUp/src/app/challenges/challenges.page.ts
+++ LevelUp/src/app/challenges/challenges.page.ts
@@ -4,6 +4,7 @@ import {Challenge} from '../models/Challenge';
 import {ChallengeServiceProvider} from '../providers/challenge-service/challenge-service';
 import {ActivityType, CarbonFootprintArgs, Country, FuelType, Mode} from "../carbon-footprint/carbonFootprintArgs";
 import {CarbonFootprintAccessor} from "../carbon-footprint/carbonFootprintAccessor";
+import {HttpClient} from '@angular/common/http';
 
 @Component({
     selector: 'app-challenges',
@@ -19,7 +20,7 @@ export class ChallengesPage implements OnInit {
     ngOnInit() {
     }
 
-    constructor(private challengeServiceProvider: ChallengeServiceProvider) {
+    constructor(private challengeServiceProvider: ChallengeServiceProvider, public http: HttpClient) {
         this.challengeServiceProvider.getChallenges().then((challenges => {
             this.challenges = challenges;
             this.filteredChallenges = challenges;
@@ -55,7 +56,7 @@ export class ChallengesPage implements OnInit {
         args.fuelType = FuelType.motorGasoline;
         args.mode = Mode.petrolCar;
 
-        let carbonFootprintAccessor = new CarbonFootprintAccessor();
+        let carbonFootprintAccessor = new CarbonFootprintAccessor(this.http);
         let response = carbonFootprintAccessor.getCarbonFootprint(args);
         console.log(response);
     }
