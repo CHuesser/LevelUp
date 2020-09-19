5efa06a5110d2980ac96848aaacde3df4fda471d
simon.beck@fhnw.ch <simubeck@hotmail.com>
2020-09-19 14:06:42 +0200

    add and remove friends

--- LevelUp/src/app/community/community.page.ts
+++ LevelUp/src/app/community/community.page.ts
@@ -4,6 +4,9 @@ import { Group } from '../models/group';
 import { User } from '../models/user';
 import { GroupServiceProvider } from '../providers/group-service/group-service';
 import { UserServiceProvider} from '../providers/user-service/user-service';
+import { ModalController } from '@ionic/angular';
+import {ModalPagePage} from '../modal-page/modal-page.page';
+import { UserlistPage } from '../userlist/userlist.page';
 
 @Component({
     selector: 'app-community',
@@ -22,11 +25,17 @@ export class CommunityPage implements OnInit {
         this.updateGroupList();
     }
 
-    constructor( private userServiceProvider: UserServiceProvider, private groupServiceProvider: GroupServiceProvider) {
+    constructor( private userServiceProvider: UserServiceProvider, private groupServiceProvider: GroupServiceProvider, public modalCtrl: ModalController) {
         this.userServiceProvider.getUser(0)
             .then(me => this.me = me)
             .then(_ => this.updateFriendList())
             .then(_ => this.updateGroupList());
+    }
+
+    removeFriend(userId) {
+        let index = this.me.friendIds.indexOf(userId);
+        this.me.friendIds.splice(index, 1);
+        this.updateFriendList();
     }
 
     updateFriendList() {
@@ -37,5 +46,18 @@ export class CommunityPage implements OnInit {
     updateGroupList() {
         this.groupServiceProvider.getGroupsToUser(this.me)
             .then(groups => this.groups = groups);
+    }
+
+    async openUserlist() {
+        let userlistPage = await this.modalCtrl.create({
+            component: UserlistPage
+        });
+
+        userlistPage.onWillDismiss()
+            .then((data) => {
+                this.updateFriendList();
+            });
+
+        return await userlistPage.present();
     }
 }
--- LevelUp/src/app/userlist/userlist.page.ts
+++ LevelUp/src/app/userlist/userlist.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../models/user';
-import { UserServiceProvider} from '../providers/user-service/user-service';
+import { UserServiceProvider } from '../providers/user-service/user-service';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-userlist',
@@ -12,20 +13,28 @@ export class UserlistPage implements OnInit {
   private me: User;
   private nonFriends: User[];
 
-  constructor(private activatedRoute: ActivatedRoute, private userServiceProvider: UserServiceProvider) { }
+  constructor(private activatedRoute: ActivatedRoute, private userServiceProvider: UserServiceProvider, public modalController: ModalController) { }
 
   ngOnInit() {
     let meid = +this.activatedRoute.snapshot.paramMap.get('meid');
-    this.userServiceProvider.getAllUsers().then(users => {
-      this.userServiceProvider.getUser(meid).then(me => {
-        this.me = me;
-        this.nonFriends = users.filter(u => u.id !== this.me.id).filter(u => !this.me.friendIds.includes(u.id))
-      })
+    this.userServiceProvider.getUser(meid).then(me => {
+      this.me = me;
+      this.updateUsers();
     });
+  }
+
+  updateUsers() {
+    this.userServiceProvider.getAllUsers().then(users => {
+      this.nonFriends = users.filter(u => u.id !== this.me.id).filter(u => !this.me.friendIds.includes(u.id))
+    })
+  }
+
+  closeUserlist() {
+    this.modalController.dismiss();
   }
 
   addFriend(friendId: number) {
     this.me.friendIds.push(friendId);
-    this.disableButton = true;
+    this.updateUsers();
   }
 }
