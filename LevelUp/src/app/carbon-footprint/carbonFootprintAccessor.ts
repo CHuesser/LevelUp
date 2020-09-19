94e934c292804bf3b3508e20aa41c4cd94a66357
theJoeSen <45791532+theJoeSen@users.noreply.github.com>
2020-09-19 16:40:48 +0200

    added carbonFootprintApiAccessor

---
+++ LevelUp/src/app/carbon-footprint/carbonFootprintAccessor.ts
@@ -0,0 +1,19 @@
+import { CarbonFootprintArgs };
+
+export class CarbonFootprintAccessor {
+    apiPrefix = 'https://api.triptocarbon.xyz/v1/footprint?';
+
+
+    getCarbonFootprint(args: CarbonFootprintArgs): Promise<CarbonFootprintResponse>{
+        // example: https://api.triptocarbon.xyz/v1/footprint?activity=10&activityType=miles&country=def&mode=taxi
+        const url = new URL(this.apiPrefix + 'activity=' + args.activity +
+            '&activityType=' + args.activityType + '&country=' + args.mode);
+        return fetch(url.toString()).then(res => res.json()).then(res => res as CarbonFootprintResponse);
+    }
+}
+
+
+
+export class CarbonFootprintResponse{
+    carbonFootprint: number;
+}
